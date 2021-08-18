b7a3e84403d75874e70ec7692676c077f66d974f
WataNekko <88185666+WataNekko@users.noreply.github.com>
2021-08-18 18:09:35 +0700

    Update Servo

--- VDrone/Assets/Scripts/Robotics/Servo/BLDCMotor.cs
+++ VDrone/Assets/Scripts/Robotics/Servo/BLDCMotor.cs
@@ -2,5 +2,11 @@ namespace Robotics.Servo
 {
     public class BLDCMotor : Servo
     {
+        private bool _isClockwise;
+
+        protected override void Update()
+        {
+            
+        }
     }
 }
--- VDrone/Assets/Scripts/Robotics/Servo/Servo.cs
+++ VDrone/Assets/Scripts/Robotics/Servo/Servo.cs
@@ -11,21 +11,32 @@ namespace Robotics.Servo
         private int _pulseWidth = 1500;
 
         /// <summary>
+        /// Sets min and max pulse width values for writes.
+        /// </summary>
+        /// <param name="min">Min pulse width</param>
+        /// <param name="max">Max pulse width</param>
+        public void attach(int min, int max)
+        {
+            if (min > max)
+            {
+                throw new System.ArgumentException("Min is larger than max value.");
+            }
+
+            (_minPulseWidth, _maxPulseWidth) = (min, max);
+        }
+
+        /// <summary>
         /// Writes pulse width in microseconds.
         /// </summary>
         /// <param name="value">Pulse width</param>
-        public void writeMicroseconds(int value)
-        {
-            _pulseWidth = Mathf.Clamp(value, _minPulseWidth, _maxPulseWidth);
-        }
+        public void writeMicroseconds(int value) => _pulseWidth = Mathf.Clamp(value, _minPulseWidth, _maxPulseWidth);
 
         /// <summary>
         /// Returns current pulse width in microseconds for this Servo.
         /// </summary>
         /// <returns>Current pulse width in microseconds.</returns>
-        public int readMicroseconds()
-        {
-            return _pulseWidth;
-        }
+        public int readMicroseconds() => _pulseWidth;
+
+        protected abstract void Update();
     }
 }
