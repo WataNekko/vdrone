e6c43abf1585bb3322986ebfc7b454d8a2c6bbec
WataNekko <88185666+WataNekko@users.noreply.github.com>
2021-08-23 19:29:14 +0700

    Update Drone's spec
    Changed mass, drag, and angular drag.

--- VDrone/Assets/Scripts/Test.cs
+++ VDrone/Assets/Scripts/Test.cs
@@ -4,6 +4,7 @@ public class Test : MonoBehaviour
 {
     Rigidbody rb;
     public Vector3 Force;
+    public Vector3 Torque;
 
     private void Start()
     {
@@ -14,5 +15,6 @@ public class Test : MonoBehaviour
     private void FixedUpdate()
     {
         rb.AddRelativeForce(Force);
+        rb.AddRelativeTorque(Torque);
     }
 }
