74ca3a9fe6c6175e71ff4cd705e6ce5e8cc5e2c2
WataNekko <88185666+WataNekko@users.noreply.github.com>
2021-09-07 19:48:40 +0700

    Update motor
    Add acceleration (higher reaction torque when speed change is large).
    Add warning if tf and rb is null on awake.
    Update force amount.
    Update drone's drag

--- VDrone/Assets/Scripts/Robotics/Servos/BLDCMotor.cs
+++ VDrone/Assets/Scripts/Robotics/Servos/BLDCMotor.cs
@@ -13,52 +13,71 @@ namespace Robotics.Servos
         }
 
         [Header("Motor's Spec")]
-        [SerializeField]
-        [Tooltip("Maximum angular speed (deg/s) at full throttle.")]
-        [Min(0f)]
+        [SerializeField, Min(0f), Tooltip("Maximum angular speed (deg/s) at full throttle.")]
         private float _maxSpeed = 120_000f;
-        [SerializeField]
-        [Tooltip("Whether the rotation direction is clockwise or counter-clockwise.")]
+        [SerializeField, Tooltip("Whether the rotation direction is clockwise or counter-clockwise.")]
         private RotationDirection _direction = RotationDirection.Clockwise;
 
         [Header("Torque & Force")]
-        [SerializeField]
-        [Tooltip("Transform of the rotor on which to apply rotation.")]
+        [SerializeField, Tooltip("Transform of the rotor on which to apply rotation.")]
         private Transform _rotorTransform;
-        [SerializeField]
-        [Tooltip("The amount of reaction torque per 1 deg/s angular speed of the rotor to apply on the rigidbody.")]
-        [Min(0f)]
+        [SerializeField, Min(0f), Tooltip("The amount of reaction torque per 1 deg/s angular speed of the rotor to apply on the rigidbody.")]
         private float _torqueAmount = 1e-6f;
-        [SerializeField]
-        [Tooltip("The vertical force produced by the motor per 1 deg/s angular speed, to be applied on the rigidbody. This value may be positive or negative to specify the force's direction.")]
+        [SerializeField, Tooltip("The vertical force produced by the motor per 1 deg/s angular speed, to be applied on the rigidbody. This value may be positive or negative to specify the force's direction.")]
         private float _forceAmount = 0f;
-        [SerializeField]
-        [Tooltip("Rigidbody on which to apply the reaction torque and vertical force produced by this motor.")]
+        [SerializeField, Tooltip("Rigidbody on which to apply the reaction torque and vertical force produced by this motor.")]
         private Rigidbody _rigidbody;
 
-        private 
+        /// <summary>
+        /// Field storing the previous angular speed with direction of the rotor.
+        /// </summary>
+        private float _prevVel;
 
         /// <summary>
-        /// Updates motor's physics.
+        /// Logs warnings if transform or rigidbody is not assigned on awake.
+        /// </summary>
+        protected virtual void Awake()
+        {
+            if (_rotorTransform == null)
+            {
+                Debug.LogWarning($"{nameof(BLDCMotor)} (ID: {GetInstanceID()}): Rotor's transform is not assigned.");
+            }
+            if (_rigidbody == null)
+            {
+                Debug.LogWarning($"{nameof(BLDCMotor)} (ID: {GetInstanceID()}): Rigidbody is not assigned.");
+            }
+        }
+
+        /// <summary>
+        /// Updates motor's rotation and physics.
         /// </summary>
         protected virtual void FixedUpdate()
         {
-            // Applies rotation on rotor
-            if (_rotorTransform != null)
+            if (_rotorTransform == null)
             {
-                float dir = (float)_direction;
-                float speed = MathUtil.MapClamped(readMicroseconds(), from: (MIN_PULSE_WIDTH, MAX_PULSE_WIDTH), to: (0f, _maxSpeed));
+                return;
+            }
 
-                _rotorTransform.Rotate(Vector3.up * (dir * speed * Time.deltaTime));
+            #region Calculate angular speed
+            float speed = MathUtil.MapClamped(readMicroseconds(), from: (MIN_PULSE_WIDTH, MAX_PULSE_WIDTH), to: (0f, _maxSpeed));
+            float currVel = (float)_direction * speed;
 
-                // Applies reaction torque and force on rigidbody
-                if (_rigidbody != null)
-                {
-                    Vector3 rotorUp = _rotorTransform.up;
+            float error = currVel - _prevVel;
+            _prevVel = currVel;
+            #endregion
 
-                    _rigidbody.AddTorque(rotorUp * (-dir * speed * _torqueAmount));
-                    _rigidbody.AddForceAtPosition(rotorUp * (speed * _forceAmount), _rotorTransform.position);
-                }
+            // Rotates rotor
+            _rotorTransform.Rotate(Vector3.up * (currVel * Time.deltaTime));
+
+            // Applies reaction torque and force on rigidbody
+            if (_rigidbody != null)
+            {
+                Vector3 rotorUp = _rotorTransform.up;
+                float acceleration = error * 100f;
+                float reactionTorque = -(currVel + acceleration) * _torqueAmount;
+
+                _rigidbody.AddTorque(rotorUp * reactionTorque);
+                _rigidbody.AddForceAtPosition(rotorUp * (speed * _forceAmount), _rotorTransform.position);
             }
         }
     }
